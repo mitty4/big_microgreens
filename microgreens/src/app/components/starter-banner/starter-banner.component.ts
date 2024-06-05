62211b79b6360921ad31374cad0b11ada4cbc123
mitty4 <malabauve@gmail.com>
2024-06-04 19:07:21 -0500

    add faq and starter-banner components

---
+++ microgreens/src/app/components/starter-banner/starter-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starter-banner',
+  templateUrl: './starter-banner.component.html',
+  styleUrls: ['./starter-banner.component.scss']
+})
+export class StarterBannerComponent {
+
+}
